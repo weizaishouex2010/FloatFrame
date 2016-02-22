7f92a6df8011eac73a777574534e33fa011b0f73
weizaishouex2010 <wupan.wp@alibaba-inc.com>
2016-02-22 14:56:32 +0800

    悬浮框可以在屏幕上自由移动，包含对状态栏高度的判断

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 + 2016-02-18 prepare tp create a float window 
 + 2016-02-22 add float window on the table, the window can show the usage rate of the devices
+
+event 事件获取位置
+![](http://dl.iteye.com/upload/attachment/500511/966205e0-edab-3bdf-bb63-fce4b869c379.png)
--- app/src/main/java/com/huangzhiwei/floatframe/FloatWindowSmallView.java
+++ app/src/main/java/com/huangzhiwei/floatframe/FloatWindowSmallView.java
@@ -8,6 +8,8 @@ import android.view.WindowManager;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import java.lang.reflect.Field;
+
 /**
  * TODO: document your custom view class.
  */
@@ -23,14 +25,54 @@ public class FloatWindowSmallView extends LinearLayout {
      */
     public static int viewHeight;
 
+    /**
+     * 用于更新小悬浮窗的位置
+     */
+    private WindowManager windowManager;
 
     /**
      * 小悬浮窗的参数
      */
     private WindowManager.LayoutParams mParams;
 
+
+    /**
+     * 记录手指位置在屏幕上的移动过程的横坐标值
+     */
+    private float xMoveInScreen;
+
+    /**
+     * 记录手指位置在屏幕上的移动过程的纵坐标值
+     */
+    private float yMoveInScreen;
+
+    /**
+     * 记录手指按下时在屏幕上的横坐标的值，即移动时初始位置的横坐标值
+     */
+    private float xDownInScreen;
+
+    /**
+     * 记录手指按下时在屏幕上的纵坐标的值，即移动时初始位置的纵坐标值
+     */
+    private float yDownInScreen;
+
+    /**
+     * 记录手指按下时在小悬浮窗的View上的横坐标的值,即相对于自身控件的横坐标值
+     */
+    private float xDownInView;
+
+    /**
+     * 记录手指按下时在小悬浮窗的View上的纵坐标的值，即相对于自身控件的纵坐标值
+     */
+    private float yDownInView;
+    /**
+     * 记录系统状态栏的高度
+     */
+    private static int statusBarHeight;
+
     public FloatWindowSmallView(Context context) {
         super(context);
+        windowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
         LayoutInflater.from(context).inflate(R.layout.float_frame_small,this);
         View view = findViewById(R.id.small_window_layout);
         viewHeight = view.getLayoutParams().height;
@@ -44,9 +86,26 @@ public class FloatWindowSmallView extends LinearLayout {
         switch (event.getAction())
         {
             case MotionEvent.ACTION_DOWN:
+                // 手指按下时记录必要数据,纵坐标的值都需要减去状态栏高度
+                xDownInScreen = event.getRawX();
+                yDownInScreen = event.getRawY()-getStatusBarHeight();
+                xMoveInScreen = event.getRawX();
+                yMoveInScreen = event.getRawY()-getStatusBarHeight();
+                xDownInView = event.getX();
+                yDownInView = event.getY();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                xMoveInScreen = event.getRawX();
+                yMoveInScreen = event.getRawY()-getStatusBarHeight();
+                // 手指移动的时候更新小悬浮窗的位置
+                updateViewPosition();
                 break;
             case MotionEvent.ACTION_UP:
-                openBigWindow();
+                // 如果手指离开屏幕时，xDownInScreen和xInScreen相等，且yDownInScreen和yInScreen相等，则视为触发了单击事件。
+                if(xMoveInScreen == xDownInScreen && yDownInScreen == yMoveInScreen)
+                    openBigWindow();
+                break;
+            default:
                 break;
         }
         return true;
@@ -69,5 +128,42 @@ public class FloatWindowSmallView extends LinearLayout {
      */
     public void setParams(WindowManager.LayoutParams params) {
         mParams = params;
+    }
+
+    /**
+     * 更新小悬浮窗在屏幕中的位置。
+     */
+    private void updateViewPosition() {
+        mParams.x = (int) (xMoveInScreen-xDownInView);
+        mParams.y = (int) (yMoveInScreen-yDownInView);
+        windowManager.updateViewLayout(this,mParams);
+    }
+
+
+    /**
+     * 用于获取状态栏的高度。
+     *
+     * @return 返回状态栏高度的像素值。
+     */
+    private int getStatusBarHeight() {
+        if(statusBarHeight==0)
+        {
+            try {
+                Class<?> c = Class.forName("com.android.internal.R$dimen");
+                Object o = c.newInstance();
+                Field field = c.getField("status_bar_height");
+                int x = (int) field.get(o);
+                statusBarHeight = getResources().getDimensionPixelSize(x);
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            } catch (InstantiationException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (NoSuchFieldException e) {
+                e.printStackTrace();
+            }
+        }
+        return statusBarHeight;
     }
 }
